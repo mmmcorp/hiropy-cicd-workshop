a10c6881af1462f27c5cffbf2172ab42894edfda
hiropy877 <kanon.hirohara@mmmcorp.co.jp>
2023-07-10 01:17:50 +0900

    [add]CodeCommitリポジトリの定義を追加

---
+++ app-cdk/bin/app-cdk.ts
@@ -0,0 +1,10 @@
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+
+const app = new cdk.App();
+
+new AppCdkStack(app, 'AppCdkStack', {});
+
+new PipelineCdkStack(app, 'pipeline-stack', {});
