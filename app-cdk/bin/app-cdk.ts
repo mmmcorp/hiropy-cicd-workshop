13b876a092eac19d33ade6cea7d7efddd75af81f
hiropy877 <kanon.hirohara@mmmcorp.co.jp>
2023-07-15 14:33:33 +0900

    [add]アプリケーション環境とECRリポジトリを追加

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,10 +1,18 @@
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AppCdkStack } from '../lib/app-cdk-stack';
-import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AppCdkStack } from "../lib/app-cdk-stack";
+import { PipelineCdkStack } from "../lib/pipeline-cdk-stack";
+import { EcrCdkStack } from "../lib/ecr-cdk-stack";
 
 const app = new cdk.App();
 
-new AppCdkStack(app, 'AppCdkStack', {});
+const ecrCdkStack = new EcrCdkStack(app, "ecr-stack", {});
 
-new PipelineCdkStack(app, 'pipeline-stack', {});
+const testAppCdkStack = new AppCdkStack(app, "test", {
+  ecrRepository: ecrCdkStack.repository,
+});
+const prodAppCdkStack = new AppCdkStack(app, "prod", {
+  ecrRepository: ecrCdkStack.repository,
+});
+
+const pipelineCdkStack = new PipelineCdkStack(app, "pipeline-stack", {});
