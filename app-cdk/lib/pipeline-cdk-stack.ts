807a4e9e1c0eec65c1d550f1b028fbc3e8669a2e
hiropy877 <kanon.hirohara@mmmcorp.co.jp>
2023-07-26 10:24:29 +0900

    [add]手順書に合わせてコメントを追加

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -5,12 +5,13 @@ import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-
+    // create a new repository.
     const sourceRepo = new codecommit.Repository(this, 'CICD_Workshop', {
       repositoryName: 'CICD_Workshop',
       description: 'Repository for my application code and infrastructure',
     });
 
+    // output repository URL
     new CfnOutput(this, 'CodeCommitRepositoryUrl', { value: sourceRepo.repositoryCloneUrlHttp });
   }
 }
